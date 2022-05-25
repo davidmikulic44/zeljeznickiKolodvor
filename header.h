c3f096713e0572bec80c0f15c87f38563324d1b1
davidmikulic44 <74313441+davidmikulic44@users.noreply.github.com>
2022-05-25 14:43:36 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,21 +1,20 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-typedef struct vlak {
-	int id;
-	char polaziste[30];
-	char odrediste[30];
-	char vrijemePolaska[6];
-	char vrijemeDolaska[6];
-}VLAK;
+	typedef struct vlak {
+		int id;
+		char polaziste[30];
+		char odrediste[30];
+		char vrijemePolaska[6];
+		char vrijemeDolaska[6];
+	}VLAK;
 
 	int izbornik();
 	void kreiranjeDatoteke();
-	void dodavanjeVlaka();
+	void dodavanjeVlaka(VLAK* poljeVlakova);
 	void ispisVoznogReda(const VLAK* const poljeVlakova);
 	void brisanjeVlaka(VLAK* poljeVlakova);
 	int zavrsetakPrograma(VLAK* poljeVlakova);
-	void pretvaranjeUCapsLock(char string[]);
-	int provjeraUnosa(char string[]);
+	
 
 #endif // HEADER_H
