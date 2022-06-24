e632137789e48c5430a8bd8ed71bec565300cbc5
davidmikulic44 <74313441+davidmikulic44@users.noreply.github.com>
2022-06-24 18:07:31 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,29 +1,33 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-	typedef struct vlak {
-		int id;
-		char polaziste[30];
-		char odrediste[30];
-		char vrijemePolaska[6];
-		char vrijemeDolaska[6];
-	}VLAK;
+typedef struct vlak {
+	int id;
+	char polaziste[30];
+	char odrediste[30];
+	char vrijemePolaska[6];
+	char vrijemeDolaska[6];
+}VLAK;
 
 
-	void* ucitavanjeVlakova();
-	void kreiranjeDatoteke();
-	void pretvaranjeUCapsLock(char string[]);
-	int provjeraUnosaStringa(char string[]);
-	int provjeraUnosaVremena(char string[]);
-	void dodavanjeVlaka(VLAK* poljeVlakova);
-	void ispisVoznogReda(VLAK* poljeVlakova);
-	void* pretrazivanjeVoznogRedaID(VLAK* poljeVlakova);
-	void* pretrazivanjeVoznogRedaPolaziste(VLAK* poljeVlakova);
-	void* pretrazivanjeVoznogRedaOdrediste(VLAK* poljeVlakova);
-	void brisanjeVlaka(VLAK* poljeVlakova);
-	int zavrsetakPrograma(VLAK* poljeVlakova);
-	int izbornikZaPretrazivanje();
-	int izbornik();
+void* ucitavanjeVlakova();
+void kreiranjeDatoteke();
+void pretvaranjeUCapsLock(char string[]);
+int provjeraUnosaStringa(char string[]);
+int provjeraUnosaVremena(char string[]);
+void dodavanjeVlaka(VLAK* poljeVlakova);
+void ispisVoznogReda(VLAK* poljeVlakova);
+void* pretrazivanjeVoznogRedaID(VLAK* poljeVlakova);
+void* pretrazivanjeVoznogRedaPolaziste(VLAK* poljeVlakova);
+void* pretrazivanjeVoznogRedaOdrediste(VLAK* poljeVlakova);
+void brisanjeVlaka(VLAK* poljeVlakova);
+int zavrsetakPrograma(VLAK* poljeVlakova);
+int izbornikZaPretrazivanje();
+int izbornikZaSortiranje();
+void sortIdSilazno(VLAK* poljeVlakova);
+void sortIdUzlazno(VLAK* poljeVlakova);
+void zamjena(VLAK* veci, VLAK* manji);
+int izbornik();
 
 
 #endif // HEADER_H
